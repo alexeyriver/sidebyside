24ca08d3033099a97c9bcd182b051d47320beb10
tymchii.a <offersbox@gmail.com>
2021-03-05 19:10:39 +0300

    front login components

--- front/src/components/login/Signin.js
+++ front/src/components/login/Signin.js
@@ -1,9 +1,47 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import './style.css'
 function Signin(props) {
+  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+  const handlerSign = (event) => {
+    const {
+      email: { value: email },
+      password: { value: password },
+    } = event.target;
+    event.preventDefault();
+    fetch(process.env.REACT_APP_URL3, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((responce) => responce.json())
+      .then((data) => {
+        if (data.success == true) {
+          localStorage.setItem('token', data.token);
+          dispatch({ type: 'AUTH_SUCCESS', payload: data.user });
+          setError('');
+        } else setError(data.message);
+      });
+  };
+  console.log(error);
   return (
-    <div>
-      
+    <div className="formWrapper">
+      <form className="formWrapper__wrapper" onSubmit={handlerSign}>
+        <label htmlFor="email">
+          Email
+        </label>
+          <input type="email" name="email" required></input>
+        <label htmlFor="password">
+          Password
+        </label>
+          <input type="password" name="password" required></input>
+        <div className="error">{error}</div>
+        <button>Sign in</button>
+      </form>
     </div>
   );
 }
--- front/src/components/login/Signup.js
+++ front/src/components/login/Signup.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 function Signup(props) {
+  const [error, setError] = useState('');
+
+  const dispatch = useDispatch();
+  const handlerSign = (event) => {
+    const {
+      username: { value: username },
+      email: { value: email },
+      password: { value: password },
+    } = event.target;
+    event.preventDefault();
+    fetch(process.env.REACT_APP_URL2, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    })
+      .then((responce) => responce.json())
+      .then((data) => {
+        if (data.success == true) {
+          localStorage.setItem('token', data.token);
+          dispatch({ type: 'REG_SUCCESS', payload: data.user });
+          setError('');
+        } else setError(data.message);
+      });
+  };
   return (
-    <div>
-      
+    <div className="formWrapper">
+      <form className="formWrapper__wrapper" onSubmit={handlerSign}>
+        <label htmlFor="username">
+          Username
+        </label>
+          <input type="text" name="username" required></input>
+        <label htmlFor="email">
+          Email
+        </label>
+          <input type="email" name="email" required></input>
+        <label htmlFor="password">
+          Password
+        </label>
+          <input type="password" name="password" required></input>
+        <div className="error">{error}</div>
+        <button>Sign up</button>
+      </form>
     </div>
   );
 }
