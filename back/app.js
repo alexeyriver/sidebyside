34f7f8da600a831983c60e928fc82ba32152056d
tymchii.a <offersbox@gmail.com>
2021-03-05 14:18:28 +0300

    routersConfig

--- back/app.js
+++ back/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import appConfig from './middleware/appConfig.js'
+import routersConfig from './middleware/routersConfig.js'
 import dbConnect from './misc/dbConnect.js'
 
 const app = express()
 
 appConfig(app)
+routersConfig(app)
 dbConnect();
 
 export default app
