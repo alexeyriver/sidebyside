d248ab490daf3534ad78bd34c8e525f8345f3674
tymchii.a <offersbox@gmail.com>
2021-03-06 14:19:38 +0300

    back, moodels schemas

--- back/models/Trip.js
+++ back/models/Trip.js
@@ -1,1 +1,10 @@
-//model
+import mongoose from 'mongoose';
+
+const tripSchema = new mongoose.Schema({
+  startDate: Date,
+  endDate: Date,
+  participants: [],
+  feedback:[]
+});
+
+export default mongoose.model('trips', tripSchema);
