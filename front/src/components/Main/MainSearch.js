0fcf34235a69897e66ed8473bcc49f6f6491e849
sasha <sanek93-93@mail.ru>
2021-03-06 15:58:37 +0300

    release 6 location onSubmit

--- front/src/components/Main/MainSearch.js
+++ front/src/components/Main/MainSearch.js
@@ -1,90 +1,66 @@
 import React from 'react';
 import Maps from './Maps'
-import DatePicker from 'react-datepicker'
-import 'react-datepicker/dist/react-datepicker.css'
-
-
-
-import SelectSerch from 'react-select-search'
 
 import { useState, useEffect } from 'react'
 import { YMaps, Map, GeoObject, Placemark } from 'react-yandex-maps';
-import ru from 'date-fns/locale/ru'
+
+
 
 
 
 function MainSearch(props) {
-  const [selectedDate, setSelectedDate] = useState(null)
-  const [selectedDateSecond, setSelectedDateSecond] = useState(null)
+
   const [value, setValue] = useState('')
   const [dataFetch, setDataFetch] = useState('')
 
   const HandlerChanger = (e) => {
-    setValue(e.target.value)
+    e.preventDefault()
+    const {town:{value:town}} = e.target
+    fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=84f3099a-6de5-4986-816c-186384023e64&format=json&geocode=${town}`)
+    .then(resp => resp.json())
+    .then(data => { if (data.response?.GeoObjectCollection.featureMember[0].GeoObject.Point.pos) { setDataFetch(data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ')) } })
+    setDataFetch('')
   }
+  console.log(dataFetch);
 
-  useEffect(() => {
-    if (value.length >= 4) {
-      fetch(`https://geocode-maps.yandex.ru/1.x/?apikey=84f3099a-6de5-4986-816c-186384023e64&format=json&geocode=${value}`)
-        .then(resp => resp.json())
-        .then(data => { if (data.response?.GeoObjectCollection.featureMember[0].GeoObject.Point.pos) { setDataFetch(data.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ')) } })//setDataFetch(data))
-    }
+  // useEffect(() => {
+  //   if (value.length >= 4) {
+  //    //setDataFetch(data))
+  //   }
 
-  }, [value])
+  // }, [value])
   
-
-
-  
-
-
-
-console.log(dataFetch);
-console.log();
+ 
 
   return (
     <div>
      <h1>ПОИСКОВАЯ ФОРМА</h1>
      
-     {/* <SelectSearch
-        options={countries}
-        search
-        filterOptions={fuzzySearch}
-        placeholder="Select your country" /> */}
+    
 
-      <div style={{ display: 'flex' }}>
+      {/* <div >
         <div className='searchDiv'>
           <input type='text' placeholder='Страна' value={value} onChange={(e) => HandlerChanger(e)}></input>
 
-          <DatePicker placeholderText="Начальная дата" selected={selectedDate}
-            onChange={date => setSelectedDate(date)}
-            dateFormat='dd.MM.yyyy'
-            minDate={new Date()}
-            isClearable
-            showYearDropdown
-            scrollableMonthYearDropdown
-            locale={ru}
-            
-          />
-
-
-
-          <DatePicker placeholderText="Конечная дата" selected={selectedDateSecond}
-            onChange={date => setSelectedDateSecond(date)}
-            dateFormat='dd.MM.yyyy'
-            minDate={selectedDate}
-            isClearable
-            showYearDropdown
-            scrollableMonthYearDropdown
-            locale={ru}
-          />
+         
 
         </div>
-      </div>
+      </div> */}
 
-      {dataFetch.length===2 && value.length>2 && <YMaps> <Map
+        <form  onSubmit={(e) => HandlerChanger(e)}>
+        <div className='searchDiv'>
+          <input name="town" type='text' placeholder='Страна' ></input>
+        <button>найти</button>
+        </div>
+      </form>
+
+
+
+
+      {dataFetch.length===2 && <YMaps> <Map
         defaultState={{
           center: [dataFetch[1],dataFetch[0]],
-          zoom: 8,
+          zoom: 10,
         }} height={500} width={700}
         // onClick={(e)=>MapHandlerClick(e._sourceEvent.originalEvent.coords)} 
         onClick={(e) => console.log(e._sourceEvent.originalEvent.coords)}
