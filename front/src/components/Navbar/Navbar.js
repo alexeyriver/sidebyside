626a4d5b3f4b1cba2db4147829e226a7149b8a30
tymchii.a <offersbox@gmail.com>
2021-03-08 16:03:37 +0300

    bootstrap-react minimum

--- front/src/components/Navbar/Navbar.js
+++ front/src/components/Navbar/Navbar.js
@@ -1,51 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Container, Row, Col, Button, Alert, Breadcrumb, Card, Form } from 'react-bootstrap'
 
 function Navbar(props) {
   const isAuth = useSelector((state) => state.auth.isAuth);
 
   return (
-    <div>
+    <Container>
       <div>
-        <div>
-          <Link to="/">Главная</Link>
-        </div>
-
-        <div>
-          <Link to="/findroute">Найти Маршрут</Link>
-        </div>
-
-        <div>
-          <Link to="/createtrip">Создать Маршрут</Link>
-        </div>
-        <div>
-          <Link to="/chat">Чат</Link>
-        </div>
-
-
-        <div>{isAuth && <Link to="/cabinet">Личный кабинет</Link>}</div>
-        <div>
-          {!isAuth && (
-            <Link className="link" to="/signup">
-              Регистрация
-            </Link>
-          )}
-        </div>
-        <div>
-          {!isAuth && (
-            <Link className="link" to="/signin">
-              Войти
-            </Link>
-          )}
-          {isAuth && (
-              <Link className="link" to="/logout">
-                Выход
-              </Link>
-          )}
-        </div>
+        <Link to="/">Главная</Link>
       </div>
-    </div>
+
+      <div>
+        <Link to="/findroute">Найти Маршрут</Link>
+      </div>
+
+      <div>
+        <Link to="/createtrip">Создать Маршрут</Link>
+      </div>
+      <div>
+        <Link to="/chat">Чат</Link>
+      </div>
+
+
+      <div>{isAuth && <Link to="/cabinet">Личный кабинет</Link>}</div>
+      <div>
+        {!isAuth && (
+          <Link className="link" to="/signup">
+            Регистрация
+          </Link>
+        )}
+      </div>
+      <div>
+        {!isAuth && (
+          <Link className="link" to="/signin">
+            Войти
+          </Link>
+        )}
+        {isAuth && (
+          <Link className="link" to="/logout">
+            Выход
+          </Link>
+        )}
+      </div>
+    </Container>
   );
 }
 
