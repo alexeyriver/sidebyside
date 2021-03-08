34d02d81b940d0d3275536a0e281d84594769736
unknown <alexeyriver@gmail.com>
2021-03-08 13:32:37 +0300

    mvp release 1.75

--- front/src/components/Map/FirstPointMap.js
+++ front/src/components/Map/FirstPointMap.js
@@ -1,32 +1,73 @@
-import React,{useState} from 'react';
-import {
-  YMaps, Map, GeoObject, Placemark,
-} from 'react-yandex-maps';
+import React, { useState, useEffect } from 'react';
+import { YMaps, Map, GeoObject, Placemark, } from 'react-yandex-maps';
+import axios from 'axios';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 
-function FirstPointMap({props}) {
-const cord=props.data.split(' ');
-  const data = useSelector((state) => state.fetch.fetchCreateJourney);
-  console.log(data);
-  console.log(cord);
-//  ПОЧЕМУ СТЕЙТ ЗАГРУЖАЕТСЯ  2 Раза?
-/////вызывать координаты из store selector
+function FirstPointMap({ props }) {
+  const [routePoint, setRoutePoint] = useState([props.data])
+  console.log(props);
+  const HandlerCreateRoute = (e) => {
+    console.log(e._sourceEvent.originalEvent.coords)
+    setRoutePoint(routePoint => routePoint = [...routePoint, e._sourceEvent.originalEvent.coords])
+  }
+  const HandlerEditRoute = (e) => {
+    console.log(e.originalEvent.target.geometry._coordinates)
+    setRoutePoint(routePoint => routePoint = routePoint.filter((el) => el != e.originalEvent.target.geometry._coordinates))
+  }
 
   return (
     <div>
       <YMaps>
-      <Map  defaultState={{
-          center: [cord[1],cord[0]],
+        <Map defaultState={{
+          center: props.data,
           zoom: 5,
         }}
-        height={500}
-        width={700}>
-{/* <Placemark geometry={[coords[1],coords[0]]} onClick={(e) => console.log(e.originalEvent.target.geometry._coordinates)} onContextMenu={(e) => console.log(e.originalEvent.target.geometry._coordinates)} /> */}
-<Placemark geometry={[cord[1],cord[0]]} onClick={(e) => console.log(e.originalEvent.target.geometry._coordinates)} onContextMenu={(e) => console.log(e.originalEvent.target.geometry._coordinates)} />
-       </Map>
-    </YMaps>
+          height={500}
+          width={700}
+
+          onClick={(e) => HandlerCreateRoute(e)}
+        >
+
+          {/* <Placemark geometry={[coords[1],coords[0]]} onClick={(e) => console.log(e.originalEvent.target.geometry._coordinates)} onContextMenu={(e) => console.log(e.originalEvent.target.geometry._coordinates)} /> */}
+
+          {routePoint && routePoint.map(el =>
+            <Placemark key={el} geometry={el}
+              onClick={(e) =>
+                console.log(e.originalEvent.target.geometry._coordinates)
+              }
+              onContextMenu={(e) => {
+                console.log(e.originalEvent.target.geometry._coordinates);
+                HandlerEditRoute(e)
+              }
+
+              } />)}
+
+          {routePoint.length >= 2 && (
+            <GeoObject
+              geometry={{
+                type: 'LineString',
+                coordinates: routePoint,
+              }}
+              options={{
+                geodesic: true,
+                strokeWidth: 5,
+                strokeColor: '#F008',
+                openBalloonOnClick: true,
+              }}
+              onClick={(e) => console.log(e.originalEvent.target.geometry._coordPath._coordinates)}
+            />
+          )}
+        </Map>
+      </YMaps>
+
+
+
+
+
+
+      
     </div>
   );
 }
