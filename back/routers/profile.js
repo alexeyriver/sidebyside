1e11a126778a16803d739ec538d1b4acae7b88fd
Oleg <shpadio@gmail.com>
2021-03-09 19:01:17 +0300

    fixes

--- back/routers/profile.js
+++ back/routers/profile.js
@@ -1,9 +1,38 @@
 import express from 'express'
 import User from "../models/User.js";
+import multer from "multer";
 const router = express.Router()
+
+const DIR = './public/';
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, DIR);
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname;
+        cb(null, fileName);
+    },
+});
+const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+        }
+    },
+});
 
 
 router.route('/:id')
+    .post(upload.single('file'), async (req, res) => {
+        const {id} = req.params
+        const user = await User.findByIdAndUpdate(id,{avatar:`http://localhost:4000/public/${req.file.filename}`})
+        res.json(user);
+    })
+
 .put(async (req, res) => {
         const { id } = req.params;
         const { name, email } = req.body;
