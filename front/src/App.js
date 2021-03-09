5da45ff5d2e3351225e948c59ebd1ea57f5c7d3c
sasha <sanek93-93@mail.ru>
2021-03-09 14:41:28 +0300

    trips

--- front/src/App.js
+++ front/src/App.js
@@ -1,63 +1,74 @@
-import  {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    Redirect
-} from 'react-router-dom';
-import MainPage from './components/Main/MainPage';
-import MainSearch from './components/Main/MainSearch';
-import CreateTrip from './components/Main/CreateTrip';
-import Navbars from './components/Navbar/Navbar';
-import Cabinet from './components/Cabinet/Cabinet';
-import {useDispatch, useSelector} from "react-redux";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import MainPage from "./components/Main/MainPage";
+import MainSearch from "./components/Main/MainSearch";
+import CreateTrip from "./components/Main/CreateTrip";
+import Navbars from "./components/Navbar/Navbar";
+import Cabinet from "./components/Cabinet/Cabinet";
+import { useDispatch, useSelector } from "react-redux";
 import Logout from "./components/login/Logout";
 import Signin from "./components/login/Signin";
 import Signup from "./components/login/Signup";
-
-
-
-
+import CurrentTrips from "./components/Trips/CurrentTrips";
+import PastTrips from "./components/Trips/PastTrips";
+import MyTrips from "./components/Trips/MyTrips";
+import Trips from './components/Trips/Trips'
 
 function App() {
+  const dispatch = useDispatch();
 
+  const isAuth = useSelector((state) => state.auth.isAuth);
 
-const dispatch = useDispatch()
+  return (
+    <Router>
+      <Navbars />
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <MainPage />
+          </Route>
+          <Route path="/findroute">
+            <MainSearch />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
 
-    const isAuth = useSelector(state => state.auth.isAuth)
+          <Route path="/signin">
+            {isAuth ? <Redirect to="/" /> : <Signin />}
+          </Route>
 
+          <Route path="/logout">
+            <Logout />
+          </Route>
+          <Route path="/createtrip">
+            <CreateTrip />
+          </Route>
+          <Route path="/cabinet">
+            <Cabinet />
+          </Route>
+          <Route path="/trips">
+            <Trips/>
+          </Route>
 
-    return (
-        <Router>
-            <Navbars/>
-            <div>
-                <Switch>
-                    <Route exact path="/">
-                        <MainPage/>
-                    </Route>
-                    <Route path="/findroute">
-                        <MainSearch/>
-                    </Route>
-                    <Route path="/signup">
-                        <Signup/>
-                    </Route>
-
-                    <Route path="/signin">{isAuth ? <Redirect to='/'/> : <Signin/>}</Route>
-
-                    <Route path="/logout">
-                        <Logout/>
-                    </Route>
-                    <Route path="/createtrip">
-                        <CreateTrip/>
-                    </Route>
-                    <Route path="/cabinet">
-                        <Cabinet/>
-                    </Route>
-
-                </Switch>
-            </div>
-        </Router>
-    );
+          <Route path="/mytrips">
+            <MyTrips />
+          </Route>
+          <Route path="/current">
+            <CurrentTrips />
+          </Route>
+          <Route path="/past">
+            <PastTrips />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- front/src/components/Main/OneRegionCard.js
+++ front/src/components/Main/OneRegionCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment'
 
 function OneRegionCard({el}) {
   console.log(el);
@@ -6,6 +7,8 @@ function OneRegionCard({el}) {
     <>
     <div style={{border:'10px solid red',borderRadius:'10%',maxWidth:'700px',margin:'20px'}}>
     <div>Имя автора: {el.author.name}</div>
+    <div>Начальная дата: {moment(el.startDate).format("DD.MM.YYYY")}</div>
+     <div>Конечная дата: {moment(el.endDate).format("DD.MM.YYYY")}</div>
     <div>Бюджет рассчитаннный автором : {el.budget}</div>
     <div>Информация о мршруте: {el.tripInfo}</div>
     <div>Количество участников: {el.participants.length > 1 ? el.participants.length : 'Пока не откликнулись,успейте присоединиться первым!'}</div>
