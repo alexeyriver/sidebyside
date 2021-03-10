fac0619b240c102e47a57d2f4dac79078fd3291f
tymchii.a <offersbox@gmail.com>
2021-03-10 14:59:12 +0300

    before pull

--- front/src/components/Profile/Change.js
+++ front/src/components/Profile/Change.js
@@ -1,49 +1,47 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {addProfilePhotoFetchAC, changeInfoFetchAC} from '../../redux/Thunk/authFetchesAC'
-
+import {changeInfoFetchAC} from '../../redux/Thunk/authFetchesAC'
 
 function Change() {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.auth.user);
-    // const error = useSelector((state) => state.auth.changeError);
+    const error = useSelector((state) => state.auth.changeError);
     const [name, setName] = useState(user.name);
     const [email, setEmail] = useState(user.email);
     const [pic,setPic] = useState({file:''})
+
 
     const onFileChange = (e) => {
         setPic({ file: e.target.files[0] });
     };
 
-    const changeInfoHandler = (e) => {
-        e.preventDefault()
-        dispatch(changeInfoFetchAC(name,email,user))
-    }
-
-
-
-    const profilePhotoHandler = (event) => {
+    const changeHandler = (event) => {
         event.preventDefault();
+        const {
+            name:{value:name},
+            email:{value:email},
+            } = event.target
         const formData = new FormData();
+        formData.append('name', name);
+        formData.append('email', email);
         formData.append('file', pic.file);
-        formData.append('id', user._id)
-        dispatch(addProfilePhotoFetchAC(formData));
+        formData.append('id', user._id);
+        dispatch(changeInfoFetchAC(formData));
     };
 
 
-    return (<div>
-            <form onSubmit={changeInfoHandler}>
-                <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Изменить имя"/>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Изменить почту"/>
-                <button  type='submit'>Записать</button>
-            </form>
+    return (
+        <div>
 
-            <form onSubmit={profilePhotoHandler}>
+            <form onSubmit={changeHandler}>
+
+                <input name="name" onChange={(e) => setName(e.target.value)} placeholder="Изменить имя"/>
+                <input name="email" onChange={(e) => setEmail(e.target.value)} placeholder="Изменить почту"/>
+
                 <input type="file" name="file" onChange={onFileChange}/>
-                <button>Добавить фото</button>
+                <button type='submit'>Записать</button>
             </form>
-           {/*<div>{error}</div>*/}
-
+           {/* <div>{error}</div> */}
         </div>
 
     );
