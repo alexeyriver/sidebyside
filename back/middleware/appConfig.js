e99f636376610933470d224e3fc77cd2083dc115
unknown <alexeyriver@gmail.com>
2021-03-11 18:09:16 +0300

    mvp 1.9975

--- back/middleware/appConfig.js
+++ back/middleware/appConfig.js
@@ -2,15 +2,21 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
+import path from 'path'
 
+// app.use(express.static(path.resolve('../front/build ? /')))
 
 const appConfig = (app) => {
-    dotenv.config()
-    app.use(cors())
-    app.use(morgan('dev'));
-    app.use('/public', express.static('public'));
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
+  dotenv.config()
+  app.use(cors())
+  app.use(morgan('dev'));
+  app.use(express.static(path.resolve('../front/build/')))
+  app.use('/public', express.static('public'));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('../front/build/index.html'))
+  })
 }
 
 
